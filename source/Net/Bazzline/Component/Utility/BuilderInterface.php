7e095d0033cdf4e311e712f21a617517381ba7b0
stevleibelt <stev@leibelt.de>
2013-08-01 23:58:37 +0200

    Created builder interface

--- source/Net/Bazzline/Component/Utility/BuilderInterface.php
+++ source/Net/Bazzline/Component/Utility/BuilderInterface.php
@@ -7,19 +7,23 @@
 namespace Net\Bazzline\Component\Utility;
 
 /**
- * Class FactoryInterface
- * Take a look to: https://en.wikipedia.org/wiki/Factory_pattern
+ * Class BuilderInterface
+ * Take a look to: https://en.wikipedia.org/wiki/Builder_pattern
+ * Add fluent interface setter methods.
  *
  * @package Net\Bazzline\Component\Utility
  * @author stev leibelt <artodeto@arcor.de>
  * @since 2013-08-01
  */
-interface FactoryInterface
+interface BuilderInterface
 {
     /**
+     * Should be the final method called after you have called all needed setter
+     *  methods.
+     *
      * @return mixed
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-08-01
      */
-    public function create();
+    public function getResult();
 }
